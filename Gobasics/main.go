ee0bcb44662c71e93335f07ea53af744e94970df
akhil <kgupta64@in.ibm.com>
2022-10-06 14:36:55 +0530

    error and interfaces

--- Gobasics/main.go
+++ Gobasics/main.go
@@ -1,57 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// functions
-
-func sumVar(nums ...int) int {
-	total := 0
-	for n := range nums {
-		total += n
+func testError(arg int) (int, error) {
+	if arg == 10 {
+		return -1, errors.New("11 is a restricion")
 	}
-	return total
+	return arg + 3, nil
 }
 
-func add(a int, b int) int {
-	return a + b
+func copyByValue(iVal int) {
+	iVal = 0
 }
 
-func addAll(a, b, c int) int {
-	return a + b + c
+func copyByPointe(iPtr *int) {
+	*iPtr = 0
 }
 
-func multiAdd(a, b string) (string, string) {
-	front := a + b
-	rev := b + a
-	return front, rev
+/*
+interfcae
+*/
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println("Area of geometry:", g.area())
+
+}
+
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 33
+	return &p
+}
+
+type rect struct {
+	width, height float64
 }
 
 func main() {
 
-	nums := []int{1, 2, 3, 4}
-	var sumAllNum = sumVar(nums...)
-	fmt.Println(" Sum is========================== ", sumAllNum)
-
-	a, b := multiAdd("akhil", "gupta")
-	fmt.Println("front ", a)
-	fmt.Println("reverse ", b)
-
-	add := add(3, 4)
-	fmt.Println("sum is", add)
-
-	addAll := addAll(3, 4, 9)
-	fmt.Println("sum All  is", addAll)
-
-	m := make(map[string]int)
-	m["akkhil"] = 1
-	m["gupta"] = 2
-	fmt.Println(">>>", m)
-	n := map[string]int{"india": 1, "hyd": 2}
-	fmt.Println("modified map ", n)
-
-	// range is used as iterator
-	for k, v := range n {
-		fmt.Println("%s -> %d\n", k, v)
+	_, answer := testError(10)
+	if answer != nil {
+		fmt.Println(" res is>>>>>>>>>>>>>> : ", answer)
+	} else {
+		fmt.Println(" error ...............")
 	}
+
+	r := rect{width: 10, height: 5}
+	fmt.Println("area---------------> ", r.area())
+
+	fmt.Println("person: ", person{"akkhl", 30})
+	//fmt.Println("person: ", )
+
+	pp := person{"gupta", 44}
+
+	fmt.Println(" first ", pp)
+	pp.age = 55
+	fmt.Println(" sec ", pp)
+
+	//fmt.Println("")
+
+	fmt.Println("<--------------------------------->")
+
+	i := 100
+
+	fmt.Println("1:1 ", i)
+
+	copyByValue(i)
+	fmt.Println("1:3: ", i)
+
+	fmt.Println("2:1 ", i)
+
+	copyByPointe(&i)
+
+	fmt.Println("2:3 ", i)
 
 }
